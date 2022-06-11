cff3e62fdd4085eba31e88fbca899b2f653ab2d3
pgstsnghiadoan <nghiadoan2k@gmail.com>
2022-06-11 16:55:15 +0700

    add main.py

--- main.py
+++ main.py
@@ -9,22 +9,25 @@ import pandas as pd
 from glob import glob
 import matplotlib.pyplot as plt
 from librosa import display
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-data_dir = './audio-folder/ducati'
+data_dir = './audio-folder/honda'
 audio_files = glob(data_dir + '/*.wav')
 count = 0
 temp = []
 # Read wav
 for i in range(0, len(audio_files) - 1):
-    y, sr = librosa.load(audio_files[i], sr=44100)
-    flatness = librosa.feature.spectral_flatness(y)
-    flatness_new = np.ndarray.flatten(flatness)
-    for k in flatness_new:
-        j = k*10*10*10*10
-        temp.append(j)
-    plt.plot(range(1, len(temp) + 1), temp)
-    plt.show()
-    temp = []
+    y, sr = librosa.load(audio_files[5], sr=44100)
+    centroid = librosa.feature.spectral_centroid(y, sr=sr)[0]
+    print(centroid)
+    print(len(centroid))
+    break
+    # flatness = librosa.feature.spectral_flatness(y)
+    # flatness_new = np.ndarray.flatten(flatness)
+    # for k in flatness_new:
+    #     j = k*10*10*10*10
+    #     temp.append(j)
+    # plt.plot(range(1, len(temp) + 1), temp)
+    # plt.show()
+    # temp = []
 
 
